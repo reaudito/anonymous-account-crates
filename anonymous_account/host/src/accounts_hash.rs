19dcdfcab2642fd24ba012fea25e25e19acfdcda
Amiya Behera <amiyatulu@gmail.com>
2024-10-22 16:18:14 +0530

    account hash

--- anonymous_account/host/src/accounts_hash.rs
+++ anonymous_account/host/src/accounts_hash.rs
@@ -1,4 +1,17 @@
+use sp_io::hashing::blake2_256;
+use subxt::config::substrate::AccountId32;
 use subxt_signer::{bip39::Mnemonic, sr25519::Keypair};
+
+fn update_hash_incrementally(current_hash: [u8; 32], account_id: &AccountId32) -> [u8; 32] {
+    let mut input_data = Vec::new();
+
+    // Extend input data with the current hash and the new account ID
+    input_data.extend_from_slice(&current_hash);
+    input_data.extend_from_slice(account_id.as_ref());
+
+    // Recalculate the hash with the new account ID
+    blake2_256(&input_data)
+}
 
 pub fn keypair_func() {
     let mut phrases = Vec::new();
@@ -12,12 +25,15 @@ pub fn keypair_func() {
     phrases.push("resemble timber picnic stage must video amount price sport help good stable");
 
     let mut account_addresses = Vec::new();
-
+    let mut current_hash: [u8; 32] = [0; 32];
     for phrase in phrases {
         let mnemonic = Mnemonic::parse(phrase).unwrap();
         let keypair = Keypair::from_phrase(&mnemonic, None).unwrap();
         let account_address = keypair.public_key().to_account_id();
         println!("{:?}", account_address);
-        account_addresses.push(account_address);
+        account_addresses.push(account_address.clone());
+        current_hash = update_hash_incrementally(current_hash, &account_address);
     }
+
+    println!("current_hash:{:?}", current_hash);
 }
